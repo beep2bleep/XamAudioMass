7bdabbf76f0c32c2ac79c97192b01e01df661693
Mark S <stick100@hotmail.com>
2016-01-28 02:21:57 -0500

    Buttons are displaying the feeds and have the mp3 link, need to have buttons take to page where you can play the mp3s linked.

--- XamAudioMass/XamAudioMass/XamAudioMass.Droid/MainActivity.cs
+++ XamAudioMass/XamAudioMass/XamAudioMass.Droid/MainActivity.cs
@@ -9,27 +9,58 @@ using Android.OS;
 
 namespace XamAudioMass.Droid
 {
-	[Activity (Label = "XamAudioMass.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "XamAudioMass.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
-		}
-	}
+            AudioFeedRetriever feedRetriever = new AudioFeedRetriever();
+
+            base.OnCreate(bundle);
+
+            var scrollView = new ScrollView(this)
+            {
+                LayoutParameters =
+                           new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)
+            };
+
+            this.SetContentView(scrollView);
+
+            var mainLayout = new LinearLayout(this)
+            {
+                Orientation = Orientation.Vertical,
+                LayoutParameters =
+                           new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)
+
+            };
+
+            // Create a LinearLayout element
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.Orientation = Orientation.Vertical;
+
+            foreach (var feed in feedRetriever.AudioFeeds)
+            {
+                // Get our button from the layout resource,
+                // and attach an event to it
+                //Button button = FindViewById<Button> (Resource.Id.myButton);
+
+                //button.Click += delegate {
+                //	button.Text = string.Format ("{0} clicks!", count++);
+                //};
+
+
+                Button audioFeedButton = new Button(this);
+                audioFeedButton.Text = feed.Title;
+                linearLayout.AddView(audioFeedButton);
+            }
+            scrollView.AddView(linearLayout);
+
+
+        }
+    }
 }
-
-
--- XamAudioMass/XamAudioMass/XamAudioMass/MyClass.cs
+++ XamAudioMass/XamAudioMass/XamAudioMass/MyClass.cs
@@ -1,12 +1,106 @@
 ﻿using System;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Collections.Generic;
 
 namespace XamAudioMass
 {
-	public class MyClass
+	public class AudioFeedRetriever
 	{
-		public MyClass ()
+        public List<FeedItem> AudioFeeds;
+
+		public AudioFeedRetriever()
 		{
-		}
-	}
+            AudioFeeds = FeedService.GetFeedItems("https://feeds.feedburner.com/usccb/zhqs");
+        }
+
+        internal static class FeedService
+        {
+            internal static List<FeedItem> GetFeedItems(string url)
+            {
+                List<FeedItem> feedItemsList = new List<FeedItem>();
+
+                try
+                {
+                    System.Net.WebRequest webRequest = System.Net.WebRequest.Create(url);
+                    System.Net.WebResponse webResponse = webRequest.GetResponse();
+                    Stream stream = webResponse.GetResponseStream();
+                    XmlDocument xmlDocument = new XmlDocument();
+                    xmlDocument.Load(stream);
+                    XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDocument.NameTable);
+                    nsmgr.AddNamespace("dc", xmlDocument.DocumentElement.GetNamespaceOfPrefix("dc"));
+                    nsmgr.AddNamespace("content", xmlDocument.DocumentElement.GetNamespaceOfPrefix("content"));
+                    XmlNodeList itemNodes = xmlDocument.SelectNodes("rss/channel/item");
+
+                    for (int i = 0; i < itemNodes.Count; i++)
+                    {
+                        FeedItem feedItem = new FeedItem();
+
+                        if (itemNodes[i].SelectSingleNode("title") != null)
+                        {
+                            feedItem.Title = itemNodes[i].SelectSingleNode("title").InnerText;
+                        }
+                        if (itemNodes[i].SelectSingleNode("link") != null)
+                        {
+                            feedItem.Link = itemNodes[i].SelectSingleNode("link").InnerText;
+                        }
+                        if (itemNodes[i].SelectSingleNode("pubDate") != null)
+                        {
+                            //feedItem.PubDate = Convert.ToDateTime(itemNodes[i].SelectSingleNode("pubDate").InnerText);
+                        }
+                        if (itemNodes[i].SelectSingleNode("dc:creator", nsmgr) != null)
+                        {
+                            feedItem.Creator = itemNodes[i].SelectSingleNode("dc:creator", nsmgr).InnerText;
+                        }
+                        if (itemNodes[i].SelectSingleNode("category") != null)
+                        {
+                            feedItem.Category = itemNodes[i].SelectSingleNode("category").InnerText;
+                        }
+                        if (itemNodes[i].SelectSingleNode("description") != null)
+                        {
+                            feedItem.Description = itemNodes[i].SelectSingleNode("description").InnerText;
+                        }
+                        if (itemNodes[i].SelectSingleNode("content:encoded", nsmgr) != null)
+                        {
+                            feedItem.Content = itemNodes[i].SelectSingleNode("content:encoded", nsmgr).InnerText;
+                        }
+                        else
+                        {
+                            feedItem.Content = feedItem.Description;
+                        }
+                        feedItemsList.Add(feedItem);
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return feedItemsList;
+            }
+        }
+
+        public class FeedItem
+        {
+            public FeedItem()
+            {
+            }
+
+            public string Title { get; set; }
+            public string Link { get; set; }
+            public DateTime PubDate { get; set; }
+            public string Creator { get; set; }
+            public string Category { get; set; }
+            public string Description { get; set; }
+            public string Content { get; set; }
+        }
+
+
+    }
+
+
+
+    
 }
 
